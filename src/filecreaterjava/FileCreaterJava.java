4ae9b18b5b33a783f4c4ee81c8ff573f93af7e3e
dubem4521@gmail.com <st10036050@vcconnect.edu.za>
2025-10-16 08:15:01 +0200

    Created a new FileCreater program it just has the JOptionPane to enter the the file name and path the user wants

---
+++ src/filecreaterjava/FileCreaterJava.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package filecreaterjava;
+
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author maxwe
+ */
+public class FileCreaterJava {
+
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String newFile = JOptionPane.showInputDialog(null, "Where would you like to store the file (Enter Path)");
+        
+        
+    }
+    
+    
+}
